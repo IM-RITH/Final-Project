c3a7d33956ae6ed09971504c0c535026e1a105c1
RITH <srey.sovanrithhust@gmail.com>
2024-03-23 00:18:04 +0700

    getUserCurrentLocation

--- lib/views/screens/map/map_screen.dart
+++ lib/views/screens/map/map_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import "package:geolocator/geolocator.dart";
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -14,10 +15,33 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  late GoogleMapController mapController;
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
-  );
+  ); // google headquarter
+
+  late Position currentLocation;
+  // get user current location
+  getUserCurrentLocation() async {
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation,
+      forceAndroidLocationManager: true,
+    );
+    currentLocation = position;
+
+    // create variable to store latitude and longitude
+    LatLng latPos = LatLng(position.latitude, position.longitude);
+
+    // set the map's initial camera position to the user's current location
+    CameraPosition cameraTarget = CameraPosition(target: latPos, zoom: 20);
+
+    // move the map's camera to the new CameraPosition
+    mapController.moveCamera(CameraUpdate.newCameraPosition(cameraTarget));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +55,15 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           GoogleMap(
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
             padding: const EdgeInsets.only(bottom: 100),
             mapType: MapType.normal,
             initialCameraPosition: _kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
+              mapController = controller;
+              getUserCurrentLocation();
             },
           ),
           Positioned(
